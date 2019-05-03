16854bd933c65d11dc0014886b4e55150b3176d5
Kushiro Taichi <k.taichi0315@gmail.com>
2019-05-03 14:57:29 +0900

    add table Book

--- backend/shelves/models.py
+++ backend/shelves/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.CharField(max_length=256, primary_key=True, unique=True)
+    title = models.CharField(max_length=256)
+    cover_url = models.CharField(max_length=256,blank=True)
+
+    def __str__(self):
+        return self.title
