b9d333ece44b8f888ea2ccf491044bc66f4bbcfe
Kushiro Taichi <k.taichi0315@gmail.com>
2019-05-03 15:08:12 +0900

    add router api/books

--- backend/shelves/views.py
+++ backend/shelves/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import BookSerializer      
+from .models import Book                  
 
-# Create your views here.
+class BookView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
