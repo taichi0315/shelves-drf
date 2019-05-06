785f4ea2b56078192c592660f182a558cb0dcece
taichi0315 <k.taichi0315@gmail.com>
2019-05-06 23:53:01 +0900

    add docker tag & ALLOWED_HOST to *

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@9f!zsqvdyfcrvhm@ow@6#pc7@85smu(g*$(oge@cg-4^yrp^c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
